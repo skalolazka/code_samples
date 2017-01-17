b681d0e75ef087e4a2098dcdea67b0cb4c9cc1a6
Наталья <flore@skalolazka-osx2.speedportentry2090126200030>
2017-01-17 16:36:59 +0100

    Initial commit.

---
+++ encode_decode.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+
+# Task: 1. Encode a string so that every substring of same letters turns into a substring
+# of the count of them and the letter itself ("aaabb" => "3a2b")
+# 2. Decode
+
+use strict;
+use warnings;
+
+package MyEncoding;
+
+sub new {
+    return bless {} => shift;
+}
+
+sub encode {
+    my ($self, $str) = @_;
+    my @str = split(//, $str);
+    my ($prev, $count, $encoded) = ($str[0], 1, '');
+
+    for (my $j = 1; $j < scalar(@str); $j++) {
+        if ($str[$j] ne $prev) {
+            $encoded .= "$count$prev";
+            $count = 0;
+        }
+        $count++;
+        $prev = $str[$j];
+    }
+    $encoded .= $prev ? "$count$prev" : '';
+    return $encoded;
+}
+
+sub decode {
+    my ($self, $str) = @_;
+    my @str = split(//, $str);
+    my ($letter, $count, $decoded) = ('', 0, '');
+
+    for (my $j = 0; $j < scalar(@str); $j += 1) {
+        if ($str[$j] =~ /\d/) { # count
+            $count .= $str[$j];
+        }
+        else { # letter
+            $letter = $str[$j];
+            $decoded .= $letter x $count;
+            $count = '';
+        }
+    }
+
+    return $decoded;
+}
+
+package TestMyEncoding;
+use Test::More;
+use Test::Deep;
+
+my $module = MyEncoding->new;
+is($module->encode(''), '', 'empty str OK');
+is($module->encode('a'), '1a', 'a OK');
+is($module->encode('aaa'), '3a', 'aaa OK');
+is($module->encode('aacccb'), '2a3c1b', 'aaccb OK');
+is($module->encode('aaaacbbb'), '4a1c3b', 'aaaacbbb OK');
+is($module->decode(''), '', 'decode: empty str OK');
+is($module->decode('1a'), 'a', 'decode: 1a OK');
+is($module->decode('2a'), 'aa', 'decode: 2a OK');
+is($module->decode('3a1c2b6d'), 'aaacbbdddddd', 'decode: 3a1c2b6d OK');
+is($module->decode('1a2b1a'), 'abba', 'decode: 1a2b1a OK');
+is($module->decode('11z'), 'zzzzzzzzzzz', 'decode: 2a OK');
+
+
+done_testing;
---
+++ my_bintree.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+# Task: print binary tree in level order
+
+from collections import deque
+
+
+class MyBinTree:
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def put_left(self, value):
+        if isinstance(value, MyBinTree): # let's leave it this simple for now
+            self.left = value
+        elif value is not None:
+            self.left = self.__class__(value) # for inheritance
+
+    def put_right(self, value):
+        if isinstance(value, MyBinTree): # let's leave it this simple for now
+            self.right = value
+        elif value is not None:
+            self.right = self.__class__(value) # for inheritance
+
+
+def array_by_levels(tree):
+    if tree is None:
+        return []
+    cur = tree
+    q = deque()
+    res = []
+    q.append(cur)
+    while q:
+        cur = q.popleft()
+        res.append(cur.value)
+        if cur.left is not None:
+            q.append(cur.left)
+        if cur.right is not None:
+            q.append(cur.right)
+    return res
+
+import unittest
+
+class TestMyBinTree(unittest.TestCase):
+    def test_init(self):
+        t = MyBinTree()
+        self.assertIsInstance(t, MyBinTree, 'object created')
+        self.assertEqual(t.value, None, 'no value')
+        self.assertEqual(t.left, None, 'no left')
+        self.assertEqual(t.right, None, 'no right')
+        t = MyBinTree(3)
+        self.assertIsInstance(t, MyBinTree, 'object created')
+        self.assertEqual(t.value, 3, 'value ok')
+        self.assertEqual(t.left, None, 'no left still')
+        self.assertEqual(t.right, None, 'no right still')
+
+    def test_put(self):
+        t = MyBinTree(2)
+        t.put_left(3)
+        self.assertEqual(t.left.value, 3, 'left value')
+        self.assertEqual(t.left.left, None, 'no left.left')
+        self.assertEqual(t.left.right, None, 'no left.right')
+        self.assertEqual(t.right, None, 'no right')
+        t.put_right(MyBinTree(5))
+        self.assertIsInstance(t.right, MyBinTree, 'object created')
+        print t.right.value
+        self.assertIsInstance(t.right.value, int, 'normal int value')
+        self.assertEqual(t.right.value, 5, 'right value')
+        self.assertEqual(t.right.left, None, 'no right.left')
+        self.assertEqual(t.right.right, None, 'no right.right')
+
+    def test_print_by_levels(self):
+        self.assertEqual(array_by_levels(None), [], 'none')
+        t = MyBinTree()
+        self.assertEqual(array_by_levels(t), [None], 'empty tree')
+        t1 = MyBinTree(4)
+        self.assertEqual(array_by_levels(t1), [4], 'one')
+        #      1
+        #     / \
+        #    2   3
+        #   /\    \
+        #  4  5    6
+        # /    \    \
+        #7      8    9
+        t1.put_left(7)
+        print array_by_levels(t1)
+        self.assertEqual(array_by_levels(t1), [4,7], 'two')
+        t2 = MyBinTree(5)
+        t2.put_right(8)
+        self.assertEqual(array_by_levels(t2), [5,8], 'two (right)')
+        t3 = MyBinTree(2)
+        t3.put_left(t1)
+        t3.put_right(t2)
+        tm = MyBinTree(1)
+        tm.put_left(t3)
+        self.assertEqual(array_by_levels(tm), [1,2,4,5,7,8], 'one left subtree')
+        t4 = MyBinTree(6)
+        t4.put_right(9)
+        t5 = MyBinTree(3)
+        t5.put_right(t4)
+        tm.put_right(t5)
+        self.assertEqual(array_by_levels(tm), [1,2,3,4,5,6,7,8,9], 'printed long')
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestMyBinTree)
+    unittest.TextTestRunner(verbosity=2).run(suite)
